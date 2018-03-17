1fcddf0878e467f2cad68ca82e58babcad052033
Redistributable <a32real@yahoo.co.jp>
2018-03-17 14:35:17 +0900

    (・_・)

--- MalinesCommon/Windows/ModernStyle/InternalElements/ModernWindowViewModel.cs
+++ MalinesCommon/Windows/ModernStyle/InternalElements/ModernWindowViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Media;
 
 namespace MalinesCommon.Windows.ModernStyle.InternalElements
@@ -64,6 +65,15 @@ namespace MalinesCommon.Windows.ModernStyle.InternalElements
             get => this.GetValue<string>();
             set => this.SetValue(value);
         }
+
+        /// <summary>
+        /// Binding property.
+        /// </summary>
+        public FrameworkElement Binding_MainElement
+        {
+            get => this.GetValue<FrameworkElement>();
+            set => this.SetValue(value);
+        } 
 
 
         // Constructor
